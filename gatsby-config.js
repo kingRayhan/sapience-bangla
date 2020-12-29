ac4e74e67fc5bbb70726736bf83c3d1c65941ee8
kingrayhan <rayhan095@gmail.com>
2020-12-29 19:49:35 +0600

    wip: অনুবাদ প্রসঙ্গে

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,20 @@ module.exports = {
       options: {
         root: __dirname,
         siteName: "সেপিয়েন্স - মানুষের ইতিহাস",
+        menuTitle: "Apollo Platform",
         description: "A brief history of humankind",
+        githubRepo: "kingrayhan/sapience-bangla",
         siteMetadata: {
           title: "সেপিয়েন্স - মানুষের ইতিহাস",
           description: "A brief history of humankind",
         },
         sidebarCategories: {
-          null: ["index", "মূল-বই-প্রসঙ্গে", "ইতিহাসের-দিনলিপি"],
+          null: [
+            "index",
+            "মূল-বই-প্রসঙ্গে",
+            "অনুবাদ প্রসঙ্গে",
+            "ইতিহাসের-দিনলিপি",
+          ],
           "বুদ্ধিভিত্তিক বিপ্লব": ["chapter1/নিতান্ত-সাধারণ-একটি-প্রাণীর-গল্প"],
         },
       },
